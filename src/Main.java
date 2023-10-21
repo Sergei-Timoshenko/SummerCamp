7c579793a0f17a226c22902fdc9b826fd855e748
Serhii Tymoshenko <sergei.timoshenko@pm.me>
2023-10-21 23:01:13 +0200

    Version 2.0

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
+import characters.Cook;
+import characters.statuses.Roles;
+
+import java.util.List;
+import java.util.Random;
+
 import timer.TickTimer;
 
 public class Main {
     public static void main(String[] args) {
+        Random random = new Random(System.nanoTime());
+        Cook cook = new Cook("Cook", Roles.COOK, null);
+        List<? extends Enum> actions = cook.getCharactersMethods();
+        Enum randomAction = actions.get(random.nextInt(actions.size()));
+        cook.doAction(randomAction);
 
         TickTimer customtickTimer = new TickTimer(); // в конструктор можно передать List<Person>, на которых вызвать методы
     }
